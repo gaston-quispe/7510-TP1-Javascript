0ad805730d3e1e81ecca0f6a074251c0b84d1b39
gaston-quispe <gastonquispe@gmail.com>
2017-10-09 15:56:26 -0300

    Refactorizacion de ConsultaParametrica

--- src/consultaParametrica.js
+++ src/consultaParametrica.js
@@ -3,32 +3,15 @@ var Consulta = require('../src/consulta');
 var ConsultaParametrica = function (nombre, parametros) {
 	// EJEMPLO SEGUN: "padre(Y, X)"
 	// "padre"
-	this.nombre = nombre;
+	var nombre = nombre;
 	// ["Y", "X"]
-	this.parametros = parametros;
-
-	this.iguales = function (evaluable) {
-		if (!evaluable instanceof ConsultaParametrica)
-			return false;
-
-		if (this.nombre !== evaluable.nombre)
-			return false;
-
-		if (this.parametros.length !== evaluable.parametros.length)
-			return false;
-
-		for (var i = 0; i < this.parametros.length; i++)
-			if (this.parametros[i] !== evaluable.parametros[i])
-				return false;
-
-		return true;
-	}
+	var parametros = parametros;
 
 	this.reemplazarParametros = function (corresponencia) {
 		var valores = [];
 
-		for (var i = 0; i < this.parametros.length; i++)
-			valores.push(corresponencia[this.parametros[i]]);
+		for (var i = 0; i < parametros.length; i++)
+			valores.push(corresponencia[parametros[i]]);
 
 		return new Consulta(nombre, valores);
 	}
